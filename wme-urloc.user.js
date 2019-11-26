523703e0a6be2e2ea3ea68d58722af6d0ccb8a83
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2019-11-26 13:52:59 +0200

    Update wme-urloc.user.js
    FIX after update of WME (thanks to by_chk)

--- wme-urloc.user.js
+++ wme-urloc.user.js
@@ -1,52 +1,52 @@
 // ==UserScript==
 // @name         WME URLoc
-// @version      2018.06.01.001
+// @version      2019.11.25.002 (corrected by_chk update)
 // @description  Updates page's URL to current WME location
 // @author       turbopirate
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
 // @grant        none
 // @namespace https://greasyfork.org/users/166361
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     let wazeapi = W || window.W;
     let projI = new OpenLayers.Projection("EPSG:900913");
     let projE = new OpenLayers.Projection("EPSG:4326");
     let last = {};
-
+ 
     let updateURL = () => {
-        if (wazeapi.map.center && (last.lat !== wazeapi.map.center.lat || last.lon !== wazeapi.map.center.lon || last.zoom !== wazeapi.map.zoom)) {
-            let lonlat=(new OpenLayers.LonLat(wazeapi.map.center.lon, wazeapi.map.center.lat)).transform(projI, projE);
+        if (wazeapi.map.getCenter() && (last.lat !== wazeapi.map.getCenter().lat || last.lon !== wazeapi.map.getCenter().lon || last.zoom !== wazeapi.map.getZoom())) {
+            let lonlat=(new OpenLayers.LonLat(wazeapi.map.getCenter().lon, wazeapi.map.getCenter().lat)).transform(projI, projE);
             let lon = Math.round(lonlat.lon * 100000) / 100000;
             let lat = Math.round(lonlat.lat * 100000) / 100000;
-            let urloc = `${window.location.protocol}//${window.location.host}${window.location.pathname}?env=row&lon=${lon}&lat=${lat}&zoom=${wazeapi.map.zoom}`;
+            let urloc = `${window.location.protocol}//${window.location.host}${window.location.pathname}?env=row&lon=${lon}&lat=${lat}&zoom=${wazeapi.map.getZoom()}`;
             window.history.pushState({path:urloc}, document.title, urloc);
-            last.lat = wazeapi.map.center.lat;
-            last.lon = wazeapi.map.center.lon;
-            last.zoom = wazeapi.map.zoom;
+            last.lat = wazeapi.map.getCenter().lat;
+            last.lon = wazeapi.map.getCenter().lon;
+            last.zoom = wazeapi.map.getZoom();
         }
-
+ 
         setTimeout(updateURL, 500);
     };
-
+ 
     let waitWaze = () => {
         wazeapi = W || window.W;
-
+ 
         if (wazeapi && wazeapi.map) {
-            last.lat = wazeapi.map.center.lat;
-            last.lon = wazeapi.map.center.lon;
-            last.zoom = wazeapi.map.zoom;
-
+            last.lat = wazeapi.map.getCenter().lat;
+            last.lon = wazeapi.map.getCenter().lon;
+            last.zoom = wazeapi.map.getZoom();
+ 
             setTimeout(updateURL, 500);
             return;
         }
-
+ 
         setTimeout(waitWaze, 500);
         console.log("URLOC: Waiting...");
     };
-
+ 
     if (window.history.pushState)
         waitWaze();
     else
