520cf92a4cf282e950b10fc9eae2bbc8728683cf
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2024-02-24 12:08:00 +0200

    small clearing

--- wme-urloc.user.js
+++ wme-urloc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME URLoc
-// @version      2021.09.15.001
+// @version      2024.02.24.001
 // @description  Updates page's URL to current WME location
 // @author       turbopirate (corrected by_chk update)
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
@@ -12,16 +12,13 @@
     'use strict';
  
     let wazeapi = W || window.W;
-    let projI = new OpenLayers.Projection("EPSG:900913");
-    let projE = new OpenLayers.Projection("EPSG:4326");
     let last = {};
  
     let updateURL = () => {
         if (wazeapi.map.getCenter() && (last.lat !== wazeapi.map.getCenter().lat || last.lon !== wazeapi.map.getCenter().lon || last.zoom !== wazeapi.map.getZoom())) {
-            let lonlat=(new OpenLayers.LonLat(wazeapi.map.getCenter().lon, wazeapi.map.getCenter().lat)).transform(projI, projE);
+            let lonlat=(new OpenLayers.LonLat(wazeapi.map.getCenter().lon, wazeapi.map.getCenter().lat)).transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
             let lon = Math.round(lonlat.lon * 100000) / 100000;
             let lat = Math.round(lonlat.lat * 100000) / 100000;
-//            let urloc = `${window.location.protocol}//${window.location.host}${window.location.pathname}?env=row&lon=${lon}&lat=${lat}&zoom=${wazeapi.map.getZoom()}`;
             let urloc = `${window.location.protocol}//${window.location.host}${window.location.pathname}?env=row&lon=${lon}&lat=${lat}&zoomLevel=${wazeapi.map.getZoom()}`;
             window.history.pushState({path:urloc}, document.title, urloc);
             last.lat = wazeapi.map.getCenter().lat;
